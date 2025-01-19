d80b4e762412bd7857e8bbaedf9a104789ea73f5
Rajat Raychaudhuri <rrc.999@gmail.com>
2025-01-18 18:31:38 -0800

    Implemented Self Attention Block (GPT), Needs tuning

---
+++ src/gpt/Attention.py
@@ -0,0 +1,101 @@
+import torch
+import torch.nn.functional as F
+
+
+
+class SelfAttentionHead(torch.nn.Module):
+
+    def __init__(self, input_dim, head_size, block_size, dropout=0.2, d="cpu", *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.key = torch.nn.Linear(input_dim, head_size, bias=False).to(d)
+        self.query = torch.nn.Linear(input_dim, head_size, bias=False).to(d)
+        self.value = torch.nn.Linear(input_dim, head_size, bias=False).to(d)
+        self.dropout = torch.nn.Dropout(dropout)
+
+        self.register_buffer("tril", torch.tril(torch.ones(block_size, block_size).to(d)))
+        self.head_size = head_size
+    
+    def forward(self, x):
+
+        # p(x, "x")
+
+        k = self.key(x)      # (B, T, C) * (C, head_size) => (B, T, head_size)
+        q = self.query(x)    # (B, T, C) * (C, head_size) => (B, T, head_size)
+        v = self.value(x)    # (B, T, C) * (C, head_size) => (B, T, head_size)
+
+        # p(k, "k")
+        # p(q.transpose(-2, -1), "q.T")
+
+        # Interaction
+        wei = k @ q.transpose(-2, -1)           # (B, T, head_size) * (B, head_size, T) => (B, T, T)
+        wei = wei * (self.head_size ** -0.5)    # Scaled down weights for the softmax layer
+
+        wei = self.dropout(wei)
+
+        # p(wei, "wei")
+        # p(self.tril, "tril")
+
+        # Only talk to previous tokens in the batch
+        wei = wei.masked_fill(self.tril == 0, float("-inf"))    # (B, T, T)
+        wei = F.softmax(wei, dim=1)                             # (B, T, T)
+
+        out = wei @ v       # (B, T, T) * (B, T, head_size) = (B, T, head_size)
+        return out
+
+
+class MultiHeadSelfAttention(torch.nn.Module):
+    def __init__(self, input_dim, head_size, block_size, number_of_heads, dropout=0.2, d="cpu", *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.saHeads = [SelfAttentionHead(input_dim, head_size, block_size, d=d) for i in range(number_of_heads)]
+
+        feature_dim = head_size * number_of_heads
+        
+        # Projection layer : Why ????
+        self.projection = torch.nn.Linear(feature_dim, feature_dim).to(d)
+        self.dropout = torch.nn.Dropout(dropout)
+        self.d= d
+    
+    def forward(self, X):
+        return self.dropout(self.projection(torch.cat([sa_head(X) for sa_head in self.saHeads], dim=-1).to(self.d)))
+
+
+class FeedForward(torch.nn.Module):
+
+    def __init__(self, input_dim, output_dim, dropout=0.2, d="cpu", *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.m = torch.nn.Sequential(
+            torch.nn.Linear(input_dim, 4 * output_dim).to(d),       # As mentioned in paper, project to a 4 times higher dim space
+            torch.nn.ReLU().to(d),
+
+            # Projection layer : Why ????
+            torch.nn.Linear(4 * output_dim, output_dim).to(d),        
+            torch.nn.Dropout(dropout)
+        )
+    
+    def forward(self, X):
+        return self.m(X)
+
+
+class Block(torch.nn.Module):
+    """
+    Contains:
+    - Layernorm #1
+    - Multiple Self Attention Heads
+    - Layernorm #2
+    - Feed Forward
+    """
+
+    def __init__(self, input_dim, head_size, block_size, number_of_heads, dropout=0.2, d="cpu", *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        feature_dim = head_size * number_of_heads
+        self.layerNorm1 = torch.nn.LayerNorm(input_dim).to(d)
+        self.saHeads = MultiHeadSelfAttention(input_dim, head_size, block_size, number_of_heads, dropout=dropout, d=d)
+        self.layerNorm2 = torch.nn.LayerNorm(feature_dim).to(d)
+        self.ffwd = FeedForward(feature_dim, feature_dim, d=d)
+
+    def forward(self, X):
+        X = X + self.saHeads(self.layerNorm1(X))
+        out = X + self.ffwd(self.layerNorm2(X))
+        return out
---
+++ src/gpt/Models.py
@@ -0,0 +1,177 @@
+import torch
+import torch.nn.functional as F
+from .Attention import MultiHeadSelfAttention, FeedForward, Block
+
+
+class CustomModel(torch.nn.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+    @torch.no_grad()
+    def generate(self, context, max_characters=10, max_block_size=8, d="cpu"):
+        context = torch.tensor([context.tolist()], device=d)
+        for _ in range(max_characters):
+            input = context[:, -max_block_size:]
+            logits, _ = self(input)
+            logits = logits[:, -1, :]
+            probs = F.softmax(logits, dim=-1)
+            ix = torch.multinomial(probs, num_samples=1).to(d)
+            context = torch.cat((context, ix), dim=1).to(d)
+        
+        return context.squeeze().tolist()
+
+
+class BaseModel(CustomModel):
+    """
+    Model with an embedding, positional embedding and a linear layer
+    """
+
+    def __init__(self, vocab_length, 
+                 feature_dim, 
+                 output_dim, 
+                 block_size, 
+                 d="cpu", 
+                 *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.embedding = torch.nn.Embedding(num_embeddings=vocab_length, embedding_dim=feature_dim, device=d)
+        self.pos_embedding = torch.nn.Embedding(num_embeddings=block_size, embedding_dim=feature_dim, device=d)
+        self.liner = torch.nn.Linear(feature_dim, output_dim, bias=True)
+        self.d = d
+    
+    def forward(self, X, target=None):
+
+        X = self.embedding(X) 
+        X_pos = self.pos_embedding(torch.arange(X.shape[1], device=self.d))
+        X = X + X_pos
+
+        logits = self.liner(X)
+        loss = None
+        if target is not None:
+            B, T, C = logits.shape
+            loss = F.cross_entropy(logits.view(B*T, C), target.view(B*T))
+        return logits, loss
+
+
+
+class BaseModel_V2(CustomModel):
+    """
+    Model Contains:
+
+    - character embedding
+    - positional embedding 
+    - Multiple Self Attention Heads
+    - Feed Forward Network
+    - Linear Layer
+
+    """
+
+    def __init__(self, vocab_length, 
+                 feature_dim, 
+                 output_dim, 
+                 block_size, 
+                 n=4,
+                 d="cpu",
+                 *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.d = d
+
+        # character embedding
+        self.embedding = torch.nn.Embedding(num_embeddings=vocab_length, embedding_dim=feature_dim).to(self.d)
+
+        # Positional Embedding
+        self.pos_embedding = torch.nn.Embedding(num_embeddings=block_size, embedding_dim=feature_dim).to(self.d)
+
+        # Multiple Attention heads
+        self.saHeads = MultiHeadSelfAttention(input_dim=feature_dim, block_size=block_size, head_size=feature_dim//n, number_of_heads=n, d=self.d)
+
+        # Feed Forward
+        self.ffwd = FeedForward(feature_dim, feature_dim, d=self.d)
+
+        # Linear Layer
+        self.liner = torch.nn.Linear(feature_dim, output_dim, bias=True).to(self.d)
+
+        
+    
+    def forward(self, X, target=None):
+
+        X = self.embedding(X) 
+        X_pos = self.pos_embedding(torch.arange(X.shape[1], device=self.d))
+        X = X + X_pos
+
+        X = self.saHeads(X)
+        X = self.ffwd(X)
+
+        logits = self.liner(X)
+        loss = None
+        if target is not None:
+            B, T, C = logits.shape
+            loss = F.cross_entropy(logits.view(B*T, C), target.view(B*T))
+        return logits, loss
+    
+
+
+class BaseModel_V3(CustomModel):
+    """
+    Model Contains:
+
+    - character embedding
+    - positional embedding 
+    - Multiple Self Attention Blocks
+    - Feed Forward Network
+    - Linear Layer
+
+    """
+
+    def __init__(self, 
+                 vocab_length, 
+                 feature_dim, 
+                 output_dim, 
+                 block_size, 
+                 number_of_sa_heads=4,
+                 number_of_sa_blocks=4,
+                 dropout=0.2,
+                 d="cpu",
+                 *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.d = d
+
+        # character embedding
+        self.embedding = torch.nn.Embedding(num_embeddings=vocab_length, embedding_dim=feature_dim).to(self.d)
+
+        # Positional Embedding
+        self.pos_embedding = torch.nn.Embedding(num_embeddings=block_size, embedding_dim=feature_dim).to(self.d)
+
+        # Multiple  self attention blocks
+        self.saBlocks = torch.nn.Sequential(
+            *[Block(input_dim=feature_dim, 
+                    head_size=feature_dim//number_of_sa_heads, 
+                    block_size=block_size, 
+                    number_of_heads=number_of_sa_heads, 
+                    dropout=dropout,
+                    d=self.d) 
+             for _ in range(number_of_sa_blocks)],
+
+             torch.nn.LayerNorm(feature_dim)
+        )
+    
+        # Linear Layer
+        self.liner = torch.nn.Linear(feature_dim, output_dim, bias=True).to(self.d)
+
+    
+    def forward(self, X, target=None):
+
+        X = self.embedding(X) 
+        X_pos = self.pos_embedding(torch.arange(X.shape[1], device=self.d))
+        X = X + X_pos
+
+        # p(X, "X")
+        X = self.saBlocks(X)
+
+        logits = self.liner(X)
+        loss = None
+        if target is not None:
+            B, T, C = logits.shape
+            loss = F.cross_entropy(logits.view(B*T, C), target.view(B*T))
+        return logits, loss
